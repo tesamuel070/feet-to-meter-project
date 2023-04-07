1ce578e75a65d1e9dfce31deb606bbda5ca4482f
Samuel Tesfaye <tesamuel070@gmail.com>
2023-04-07 04:59:47 -0700

    index feet to meter

---
+++ FtoM/FtoM.cpp
@@ -0,0 +1,41 @@
+// FtoM.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// this oroject is used to conver foot to meter
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	double M;
+	double W;
+	int counter;
+
+	counter = 0;
+
+
+
+	for (W = 1.0; W <= 100; W++) {
+		M = W / 3.21;
+		cout << W << "feet is" << M << "meter.\n";
+
+		counter++;
+
+		if (counter == 10) {
+			cout << "\n";
+			counter = 0;
+		}
+	}
+	return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
